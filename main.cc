d4832356a609e821e47bec915e648613bd833cff
s0cks <asyncronous16@gmail.com>
2016-01-12 00:22:54 +0000

    Update main.cc

--- main.cc
+++ main.cc
@@ -16,14 +16,23 @@ int main(int argc, char** argv){
     p->x = 10;
     p->y = 100;
 
+    // Create the address of the y field
     FieldAddress yAddr(RAX, offsetof(point, y));
+    // Create immediate value of 10
     Immediate ten(10);
+    // Create immediate value of p
     Immediate pointImm(reinterpret_cast<int64_t>(p));
+    // Move p's immediate value into RAX
     assm.movq(RAX, pointImm);
+    // Add 10 to the value at y's address
     assm.addq(yAddr, ten);
+    // Create the address of the x field
     FieldAddress xAddr(RAX, offsetof(point, x));
+    // Add 10 to the value at x's address
     assm.addq(xAddr, ten);
+    // Return
     assm.ret();
+    // malloc & map the memory then cast it to a function pointer and call it
     assm.Finish<func>()();
 
     printf("%d\n", p->x);
